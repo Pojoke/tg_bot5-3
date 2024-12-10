3e8ac00bf336e292324f301cd57c7892d501caa9
Pojoke <pavlykdmutro@gmail.com>
2024-12-10 20:07:03 +0200

    DZ

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="telegram.php" method="post">
+   <input type="text" name="name">
+   <input type="text" name="phone">
+   <input type="text" name="email">
+   <textarea name="text" id="" cols="30" rows="10"></textarea>
+   <input type="submit" value="send">
+</form>
+</body>
+</html>
---
+++ token.php
@@ -0,0 +1,3 @@
+
+<?php
+const TOKEN='7881468618:AAEHa84HzbSx93eCqRQqSrn2o3MrI_gyfkE';
